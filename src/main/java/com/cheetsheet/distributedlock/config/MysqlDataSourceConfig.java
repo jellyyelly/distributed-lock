ca96d2cbd1e79cb0032c190c654df34d1dfc8cc9
서정식 <jsseo@wbsoft.kr>
2025-11-28 14:15:14 +0900

    feat: 멀티 데이터소스 설정 추가 (MySQL, PostgreSQL, Redis)

---
+++ src/main/java/com/cheetsheet/distributedlock/config/MysqlDataSourceConfig.java
@@ -0,0 +1,30 @@
+package com.cheetsheet.distributedlock.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class MysqlDataSourceConfig {
+
+    @Primary
+    @Bean(name = "mysqlDataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.mysql")
+    public DataSource mysqlDataSource() {
+        return DataSourceBuilder.create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+
+    @Primary
+    @Bean(name = "mysqlJdbcTemplate")
+    public JdbcTemplate mysqlJdbcTemplate() {
+        return new JdbcTemplate(mysqlDataSource());
+    }
+}
